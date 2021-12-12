2f710cce45890dd0128bf3963f81c49a814a0967
srybi <sryb@protonmail.com>
2021-12-12 12:24:21 +0100

    day1_part2

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,5 +1,8 @@
+import os
+# part1
 prev = None
 counter = 0
+print(os.getcwd())
 with open('input.txt') as input:
     for line in input:
         if prev is not None and prev < int(line):
@@ -8,3 +11,30 @@ with open('input.txt') as input:
         prev = int(line)
 
 print(counter)
+
+
+# part2
+prev = None
+result = 0
+line_conter = 0
+sum = 0
+with open('input.txt') as input:
+    for line in input:
+        sum += int(line)
+        if line_conter >= 2:    
+            delta_counter = line_conter - 3
+            with open('input.txt') as delta_input:
+                delta_sum = 0
+                for delta_line in delta_input:
+                    if delta_counter >= 0:
+                        delta_sum += int(delta_line)
+                        delta_counter -= 1
+                    else: break
+
+                window = sum - delta_sum
+                if prev is not None and prev < window:
+                    result += 1
+                prev = window
+        line_conter += 1
+
+print(result)
