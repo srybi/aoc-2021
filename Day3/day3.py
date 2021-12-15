172cb00dc9348d8956baaf439bc29724e39a1a34
srybi <sryb@protonmail.com>
2021-12-15 20:52:46 +0100

    * Day3 part2 ... im losing hope

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,57 +1,69 @@
+import os
 
 length = 0
+print(os.getcwd())
 with open("input.txt") as input:
     for line in input:
         length = len(line.rstrip("\n"))
         break
 
-
+def get_lines_as_List():
+    with open('input.txt') as input:
+        return input.read().splitlines()
 #part1
-def get_bits(most_common):
+def get_bits(most_common, input):
+
     result = ''
     for i in range(length):
         zero_counter = 0
         one_counter = 0
-        with open("input.txt") as input:
-            for line in input:
-                if line[i] == '0': 
-                    zero_counter += 1
-                else:
-                    one_counter += 1
-        if (most_common and zero_counter < one_counter) or (not most_common and zero_counter > one_counter) or (zero_counter == one_counter):
-            result += '1'
-        elif (most_common and zero_counter > one_counter) or (not most_common and zero_counter < one_counter):
-            result += '0'
+
+        for line in input:
+            if line[i] == '1': 
+                one_counter += 1
+            else:
+                zero_counter += 1
+
+        bites = zero_counter < one_counter # there are more 1 than 0
+        equal = zero_counter = one_counter # there are euqaly 1 and 0
+
+        if most_common:
+            if bites or equal:
+                result += "1"
+            else:
+                result += "0"
+        else:
+            if bites or equal:
+                result += "0"
+            else:
+                result += "1"
+
 
     return result
-    
-gamma_rate = int(get_bits(True), 2)
-epsilon_rate = int(get_bits(False), 2)
+
+input = get_lines_as_List()
+gamma_rate = int(get_bits(True, input), 2)
+epsilon_rate = int(get_bits(False, input), 2)
 
 
 
 print(gamma_rate * epsilon_rate)
 
 #part2
-most_common = get_bits(True)
-least_common = get_bits(False)
-
-def get_perfect_str(perfect):
-    with open('input.txt') as input:
-        lines = list(set(input.read().splitlines()))
-        for index in range(length):
-            for line in lines:
-                if len(lines) == 1:
-                    return lines[0]
-                if line[index] != perfect[index]:
-                    lines.remove(line)
-
-#THERE WERE DUPLICATES IN THE INPUT FILE 
-def remove_values_from_list(the_list, val):
-   return [value for value in the_list if value != val]
+def get_perfect_str(most_common):
+    lines = get_lines_as_List()
+    
+    for index in range(length):
+        perfect = get_bits(most_common, lines)
+        for line in lines[:]:
+            if len(lines) == 1:
+                print(lines)
+                return lines[0]
+            if line[index] != perfect[index]:
+                lines.remove(line)
 
 
-oxygen_generator_rating = int(get_perfect_str(most_common), 2)
-C02_srubber_rating = int(get_perfect_str(least_common), 2)
+oxygen_generator_rating = int(get_perfect_str(True), 2)
+C02_srubber_rating = int(get_perfect_str(False), 2)
 
 print(oxygen_generator_rating * C02_srubber_rating)
