e954da08a9649dd7af86edc7f2cd027bdeb1119f
srybi <sryb@protonmail.com>
2021-12-17 21:20:17 +0100

    + Day4 with tests

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,19 +1,46 @@
-def get_bingo_numbers():
-    with open("input.txt") as input:
-        nums = input.readline().rstrip("\n")
-        return nums.split(",")
-
 class Board:
     def __init__(self, board_str):
-        # init 2d-array with board str numbers
-        pass
-        # init 2d-array only false
+        self.board_array = [[int(num) for num in row.split()] for row in board_str.split("\n")]
+        self.bool_board_array = [[False for i in range(5)] for j in range(5)]
     
-    #TODO: def checkboard(self, number):
-    #TODO: def markboard(self, number):
-    #TODO: def getTotal(self):
+    def mark_board(self, number) -> bool:
+        for r in range(5):
+            for c in range(5):
+                if self.board_array[r][c] == number:
+                    self.bool_board_array[r][c] = True
+                    return True
+        return False
+
+    def check_board(self, number) -> int:
+        #check rows
+        for r in range(5):
+            if sum(self.bool_board_array[r]) == 5:
+                return self.get_total() * number
+
+        #check columns
+        transpose = list(map(list, zip(*self.bool_board_array)))
+        print(transpose)
+        for c in range(5):
+            if sum(transpose[c]) == 5:
+                return self.get_total() * number
+
+
+    def get_total(self) -> int:
+        sum = 0
+        for r in range(5):
+            for c in range(5):
+                if self.bool_board_array[r][c]:
+                    sum += self.board_array[r][c]
+        return sum
 
 #TODO: init all boards from input.txt
 #      iterate over bingo nums
 #      interate over boards
 #      mark -> check (-> get total)
+
+
+def get_bingo_numbers():
+    with open("input.txt") as input:
+        nums = input.readline().rstrip("\n")
+        return nums.split(",")
+
---
+++ Day4/test.py
@@ -0,0 +1,118 @@
+import unittest
+
+from Day4.day4 import Board
+
+class TestDay4(unittest.TestCase):
+
+    def test_init_board_array(self):
+        #given:
+        board_str = "49  0  9 90  8\n41 88 56 13 6\n17 11 45 26 75\n29 62 27 83 36\n31 78  1 55 38"
+        board_array = [ [49, 0, 9, 90, 8], 
+                        [41, 88, 56, 13, 6], 
+                        [17, 11, 45, 26, 75],
+                        [29, 62, 27, 83, 36],
+                        [31, 78,  1, 55, 38] ]
+        
+        #when
+        b = Board(board_str)
+
+        #than
+        self.assertEqual(board_array, b.board_array)
+
+    def test_init_bool_board_array(self):
+        #given:
+        board_str = "49  0  9 90  8\n41 88 56 13 6\n17 11 45 26 75\n29 62 27 83 36\n31 78  1 55 38"
+        board_bool_array = [[False, False, False, False, False], 
+                            [False, False, False, False, False], 
+                            [False, False, False, False, False],
+                            [False, False, False, False, False],
+                            [False, False, False, False, False] ]
+        
+        #when
+        b = Board(board_str)
+
+        #than
+        self.assertEqual(board_bool_array, b.bool_board_array)
+
+    def test_mark_board(self):
+        #given:
+        test_board = Board("49  0  9 90  8\n41 88 56 13 6\n17 11 45 26 75\n29 62 27 83 36\n31 78  1 55 38")
+        num = 88
+        result_board = [[False, False, False, False, False], 
+                        [False, True, False, False, False], 
+                        [False, False, False, False, False],
+                        [False, False, False, False, False],
+                        [False, False, False, False, False]]
+        result = True
+        
+        #when
+        yo = test_board.mark_board(num)
+        #than
+        self.assertEqual(result, yo)
+        self.assertEqual(result_board, test_board.bool_board_array)
+
+    def test_mark_board_edge(self):
+        #given:
+        test_board = Board("49  0  9 90  8\n41 88 56 13 6\n17 11 45 26 75\n29 62 27 83 36\n31 78  1 55 38")
+        num = 38
+        result_board = [[False, False, False, False, False], 
+                        [False, False, False, False, False], 
+                        [False, False, False, False, False],
+                        [False, False, False, False, False],
+                        [False, False, False, False, True]]
+        result = True
+        
+        #when
+        yo = test_board.mark_board(num)
+        #than
+        self.assertEqual(result, yo)
+        self.assertEqual(result_board, test_board.bool_board_array)
+
+    def test_mark_board_failed(self):
+        #given:
+        test_board = Board("49  0  9 90  8\n41 88 56 13 6\n17 11 45 26 75\n29 62 27 83 36\n31 78  1 55 38")
+        num = -1
+        result_board = [[False, False, False, False, False], 
+                        [False, False, False, False, False], 
+                        [False, False, False, False, False],
+                        [False, False, False, False, False],
+                        [False, False, False, False, False]]
+        result = False
+        
+        #when
+        yo = test_board.mark_board(num)
+        #than
+        self.assertEqual(result, yo)
+        self.assertEqual(result_board, test_board.bool_board_array)    
+        
+    def test_check_board_rows(self):
+        #given
+        test_board = Board("52 53 19 56 80\n94 33  3 78 32\n10 89 66 48 55\n99 23 88  8 39\n76 75 44 79 14")
+        num = 99
+        nums = [94, 33,  3, 78, 32, 52, num] 
+        #when
+        for n in nums:
+            test_board.mark_board(n)
+        result = test_board.check_board(num)
+        #than
+        self.assertEqual(result, sum(nums)*num)
+
+    def test_check_board_columns(self):
+        #given
+        test_board = Board("52 53 19 56 80\n94 33  3 78 32\n10 89 66 48 55\n99 23 88  8 39\n76 75 44 79 14")
+        num = 88
+        nums = [0, 88, 11, 62, 78, 83, 8, num]
+        #when
+        for n in nums:
+            test_board.mark_board(n)
+        result = test_board.check_board(num)
+        #than
+        self.assertEqual(result, sum(nums) * num)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
